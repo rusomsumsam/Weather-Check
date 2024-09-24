1bf6ba9f74d8213df839e243987e9d840b1db7e6
sumaiya <you@example.com>
2024-09-24 23:15:18 +0600

    second commit

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ function apiData(recieveData, recieveInputValue) {
     let storeData = recieveData;
     let storeInputValue = recieveInputValue.charAt(0).toUpperCase() + recieveInputValue.slice(1);//For Uppercase the first letter of user input value
     catchDiv.innerHTML= `<p class="header">${storeInputValue}</p>
-                        <p class="item"><i class="fa-solid fa-temperature-three-quarters"></i> Temperature : <span class="item_one">${storeData.current.temp_c}</span> C / <span class="item_two">${storeData.current.temp_f}</span> F</p>
-                        <p class="item"><i class="fa-solid fa-temperature-high"></i> Feels Like : <span class="item_one">${storeData.current.feelslike_c}</span> C / <span class="item_two">${storeData.current.feelslike_f}</span> F</p>
-                        <p class="item"><i class="fa-solid fa-droplet"></i> Humidity : <span class="item_single">${storeData.current.humidity}</span></p>
-                        <p class="item"><i class="fa-solid fa-wind"></i> Wind Chill : <span class="item_one">${storeData.current.windchill_c}</span> C / <span class="item_two">${storeData.current.windchill_f}</span> F</p>
-                        <p class="item"><i class="fa-regular fa-clock"></i> Country : <span class="item_single">${storeData.location.country}</span></p>
-                        <p class="item"><i class="fa-regular fa-clock"></i> Local Time : <span class="item_single">${storeData.location.localtime}</span></p>
+                        <p class="item"><span class='icon_cus'><i class="fa-solid fa-temperature-three-quarters"></i></span> Temperature : <span class="item_one">${storeData.current.temp_c}</span> C / <span class="item_two">${storeData.current.temp_f}</span> F</p>
+                        <p class="item"><span class='icon_cus'><i class="fa-solid fa-temperature-high"></i></span> Feels Like : <span class="item_one">${storeData.current.feelslike_c}</span> C / <span class="item_two">${storeData.current.feelslike_f}</span> F</p>
+                        <p class="item"><span class='icon_cus'><i class="fa-solid fa-droplet"></i></span> Humidity : <span class="item_single">${storeData.current.humidity}</span></p>
+                        <p class="item"><span class='icon_cus'><i class="fa-solid fa-wind"></i></span> Wind Chill : <span class="item_one">${storeData.current.windchill_c}</span> C / <span class="item_two">${storeData.current.windchill_f}</span> F</p>
+                        <p class="item"><span class='icon_cus'><i class="fa-regular fa-flag"></i></span> Country : <span class="item_single">${storeData.location.country}</span></p>
+                        <p class="item"><span class='icon_cus'><i class="fa-regular fa-clock"></i></span> Local Time : <span class="item_single">${storeData.location.localtime}</span></p>
                         `
 }
 
@@ -40,5 +40,3 @@ catchButton.addEventListener('click', function () {
     responseApi();
     catchInput.value = '';
 })
-
-
